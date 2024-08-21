5a00b0e47dc001464e9110e5de62694419079bd3
Aaron Cohen <ascohen0519@gmail.com>
2024-08-21 08:08:35 -0700

    add multiple top chunks functionality, minor instruction and ui flow changes

--- pages/3Document_QA.py
+++ pages/3Document_QA.py
@@ -13,21 +13,12 @@ st.title('Document Q&A')
 st.markdown(
             '''
             <span style='font-size: 12px; color:#cd6155;'>
-            This app is currently in testing and provides basic Q&A functionality only.<br>
+            This app is currently in testing and provides basic retrieval functionality only.<br>
             Upcoming changes will include optimal N chunks to pass into prompt and document specific settings for
             default chunk size.
             ''',
             unsafe_allow_html=True)
 st.write('\n')
-
-qa_prompt = '''The following is a document, and a question about the document:
-DOCUMENT: %s
-QUESTION: %s
-INSTRUCTIONS: Please answer the users question using the document above. Keep your answer ground in the facts of the
-document. If the document doesn't contain the facts to answer the question, answer "Information not found."
-ANSWER: 
-'''
-
 
 def click_button(button):
     """Sets session state button variable to True after user clicks button.
@@ -37,23 +28,21 @@ def click_button(button):
     """
     st.session_state[button] = True
 
-
 def update_question(question):
     """Sets st.session_state.question variable to the latest question entered by the user.
 
     Args:
-        question: user entered question
+        question: user entered question.
     """
     st.session_state.question = question
-
 
 def get_chunks(chunk_approach, text, chunk_size, chunk_overlap):
     """Generates list of chunks from text string based on user inputs.
 
     Args:
-        text: text corresponding to user entered document
-        chunk_size: chunk size based on user manual input (default = 1028)
-        chunk_overlap: chunk overlap based on user manual input (default = 100)
+        text: text corresponding to user entered document.
+        chunk_size: chunk size based on user manual input.
+        chunk_overlap: chunk overlap based on user manual input.
 
     Returns:
         List of document chunks.
@@ -69,7 +58,6 @@ def get_chunks(chunk_approach, text, chunk_size, chunk_overlap):
 
     return text_splitter.split_text(text)
 
-
 def get_embedding(text):
     """Generates text embedding for given text input.
 
@@ -81,12 +69,39 @@ def get_embedding(text):
     Returns:
         Embedding vector.
     """
+
     return genai.embed_content(model='models/text-embedding-004',
                                content=text,
                                task_type='retrieval_document')['embedding']
 
+def build_prompt(document_chunk_list, question):
+    """Generates prompt to pass to model based on relevant identified document chunks and user question.
 
-def find_best_chunk(question, dataframe):
+    Args:
+        document_chunk_list: list of chunks identified as best chunks from document based on user input settings.
+        question: user question.
+
+    Returns:
+        String of full prompt to pass into model.
+    """
+
+    qa_prompt = 'The following is a set of documents, and a question about the documents: \n'
+
+    for i in range(len(document_chunk_list)):
+        qa_prompt += '\nDOCUMENT NUMBER %s: %s. END DOCUMENT.\n'%(i+1, document_chunk_list[i])
+
+    qa_prompt += '\nQUESTION: %s \n'%question
+
+    qa_prompt += ('\nINSTRUCTIONS: Please review documents in order and answer the users question as soon as you find' \
+                  'the answer. If none of the documents contain the facts to answer the question, answer "Information' \
+                  'not found.\n')
+
+    qa_prompt += '\nANSWER: '
+
+    return qa_prompt
+
+
+def find_best_chunk(question, dataframe, num_top_chunks):
     """Generates text embedding for user question and finds nearest chunk embedding from dataframe.
 
     Args:
@@ -97,23 +112,23 @@ def find_best_chunk(question, dataframe):
         Text string of most similar chunk from dataframe to feed into prompt with question.
     """
     query_embedding = get_embedding(question)
-    dot_products = np.dot(np.stack(dataframe['embedding']), query_embedding)
-    idx = np.argmax(dot_products)
+    dataframe['dot_products'] = np.dot(np.stack(dataframe['embedding']), query_embedding)
+    idx = dataframe.sort_values('dot_products', ascending=False).reset_index()
 
-    return dataframe.iloc[idx]['text']
+    num_top_chunks_final = np.min([num_top_chunks, len(dataframe)])
 
+    return [idx.iloc[n].text for n in range(num_top_chunks_final)] # dataframe.iloc[idx]['text']
 
-def produce_answer(best_chunk, question):
+def produce_answer(prompt):
     """ Produces answer based on prompt input, containing best document chunk and user question.
 
     Args:
-        best_chunk: Best chunk identified from vector store.
+        document_chunk_list: List of best document chunks for question from vector store.
         question: User question.
 
     Returns:
         Model completion (answer).
     """
-    prompt = qa_prompt%(best_chunk, question)
 
     return model.generate_content(
         contents=prompt,
@@ -142,15 +157,13 @@ genai.configure(api_key=st.session_state['gemini_api_key'])
 model = genai.GenerativeModel(
     model_name='gemini-1.5-flash',
     system_instruction=[
-        'You are a helpful document question-answering service.',
-        'For any non-english queries, respond in the same language as the prompt unless \
-        otherwise specified by the user.'])
+        'You are a helpful document question-answering service.'])
 
 # Prompt user to upload text for summarization via PDF import or manual text upload.
 st.write('''
-            ### Upload Text
-            Upload your text via pdf or manual entry
-            ''')
+        ### Upload Text
+        Upload your text via pdf or manual entry
+        ''')
 
 document_type = st.selectbox('What type of document would you like to ask questions about?',
                              ['', 'Manually input text', 'PDF'],
@@ -177,14 +190,12 @@ if document_type != '':
         st.button('Upload', on_click=click_button, args=('text_entered',))
 
         if st.session_state.text_entered:
-            with st.spinner('Wait for it...'):
-                time.sleep(2)
             text_for_qa += out
 
     if len(text_for_qa) > 0:
         num_characters = len(text_for_qa)
         num_tokens = model.count_tokens(text_for_qa).total_tokens
-        num_words = len(re.findall("[a-zA-Z_]+", text_for_qa))
+        num_words = len(re.findall('[a-zA-Z_]+', text_for_qa))
         st.markdown(
             '''
             <span style='font-size: 12px;'> 
@@ -210,22 +221,28 @@ if document_type != '':
                                            'Auto',
                                            'Fixed size chunking',
                                            'Recursive chunking',
-                                           'Sentence chunking'])
+                                           'Sentence chunking'],
+                                        help='''
+                                            This determines the logic and parameters used to break your document into
+                                            chunks.
+                                            ''')
 
         if chunk_setting != '':
 
             if chunk_setting == 'Auto':
                 chunk_size = 1024
                 chunk_overlap = 20
+                num_top_chunks = 3
 
             else:
                 chunk_size = st.select_slider('How many characters per chunk?',
                                                options=[''] + list(range(1, 2001, 1)))
                 chunk_overlap = st.select_slider('How many characters to overlap between chunks?',
                                                options=[''] + list(range(0, 101, 1)))
+                num_top_chunks = st.select_slider('How many chunks to retrieve for LLM in final prompt?',
+                                               options=[''] + list(range(1, 10, 1)))
 
-            if chunk_size != '' and chunk_overlap != '':
-
+            if chunk_size != '' and chunk_overlap != '' and num_top_chunks != '':
 
                 st.write('generating chunks...')
                 chunks = get_chunks(d_model[chunk_setting], text_for_qa, chunk_size, chunk_overlap)
@@ -254,21 +271,27 @@ if document_type != '':
                     with st.spinner('Wait for it...'):
                         time.sleep(2)
 
-                    best_chunk = find_best_chunk(st.session_state.question, st.session_state.df)
+                    document_chunk_list = find_best_chunk(
+                        st.session_state.question, st.session_state.df, num_top_chunks)
 
-                    st.session_state.answer = produce_answer(best_chunk, st.session_state.question)
+                    prompt = build_prompt(document_chunk_list, st.session_state.question)
+                    st.session_state.answer = produce_answer(prompt)
 
-                    st.markdown('answer: %s'%st.session_state.answer)
+                    st.markdown(st.session_state.answer)
 
                     st.session_state.answer = ''
                     st.session_state.question = ''
 
                     with st.expander('debug'):
-                        st.write('Number of chunks: %s'%len(chunks))
-                        st.write('Avg character length of chunk: %s' % round(num_characters / len(chunks), 1))
-                        st.write('Best chunk for current question')
-                        st.write(best_chunk)
-                        st.write('first, second and last chunk')
+                        st.write('Chunk size setting: %s'%chunk_size)
+                        st.write('Chunk overlap setting: %s' % chunk_overlap)
+                        st.write('Number of chunks generated: %s'%len(chunks))
+                        st.write('Avg character length of generated chunk: %s' % round(num_characters / len(chunks), 1))
+                        st.write('Best chunks identified for current question:')
+                        st.write(document_chunk_list)
+                        st.write('first, second and last chunk:')
                         st.write(chunks[:2] + chunks[-1:])
-                        st.write('first, second and last embedding')
+                        st.write('first, second and last embedding:')
                         st.write(pd.concat([st.session_state.df.head(2), st.session_state.df.tail(1)]))
+                        st.write('Final prompt:')
+                        st.write(prompt)
